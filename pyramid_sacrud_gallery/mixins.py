b48aa2b166e96f30f82c408ea6a722c69a601764
Petr Zelenin <po.zelenin@yandex.ru>
2014-11-13 18:09:05 +0500

    Set relation M2M

--- pyramid_sacrud_gallery/mixins.py
+++ pyramid_sacrud_gallery/mixins.py
@@ -6,7 +6,7 @@
 
 from sqlalchemy import Column, Integer, String, Text
 from sqlalchemy.ext.declarative import declared_attr
-from sqlalchemy.orm import foreign, relationship, remote
+from sqlalchemy.orm import relationship
 from sqlalchemy.schema import ForeignKey
 
 
@@ -25,12 +25,21 @@ class BaseMixin(object):
     def get_col_pk(cls):
         return getattr(cls, cls.get_pk())
 
-    def get_val_pk(self):
-        return getattr(self, self.get_pk())
+    @classmethod
+    def get_ref_class_name(cls):
+        return getattr(cls, 'pyramid_sacrud_ref_name')
+
+    @classmethod
+    def get_m2m_table(cls):
+        return getattr(cls, 'pyramid_sacrud_m2m_table',
+                       GalleryItemM2MMixin.__tablename__)
 
     @declared_attr
     def __tablename__(cls):
         return cls.__name__.lower()
+
+    def get_val_pk(self):
+        return getattr(self, self.get_pk())
 
 
 class GalleryMixin(BaseMixin):
@@ -39,30 +48,48 @@ class GalleryMixin(BaseMixin):
     def __repr__(self):
         return self.name
 
+    @declared_attr
+    def items(cls):
+        return relationship(cls.get_ref_class_name(),
+                            secondary=cls.get_m2m_table())
+
 
 class GalleryItemMixin(BaseMixin):
-    path = Column(String, nullable=False)
+    name = Column(String, nullable=False)
     description = Column(Text)
 
+    @declared_attr
+    def galleries(cls):
+        return relationship(cls.get_ref_class_name(),
+                            secondary=cls.get_m2m_table())
+
+
+class GalleryItemM2MMixin(object):
+
     @classmethod
-    def get_parent_class(cls):
+    def get_gallery_class(cls):
         return getattr(cls, 'pyramid_sacrud_gallery', GalleryMixin)
+
+    @classmethod
+    def get_item_class(cls):
+        return getattr(cls, 'pyramid_sacrud_gallery_item', GalleryItemMixin)
+
+    @declared_attr
+    def __tablename__(cls):
+        return cls.__name__.lower()
 
     @declared_attr
     def gallery_id(cls):
-        gallery_cls = cls.get_parent_class()
-        fk = '{0}.{1}'.format(gallery_cls.__tablename__,
-                              gallery_cls.get_db_pk())
+        gallery_cls = cls.get_gallery_class()
         return Column(Integer,
-                      ForeignKey(fk, ondelete='CASCADE'),
+                      ForeignKey(gallery_cls.get_col_pk(),
+                                 ondelete='CASCADE'),
                       nullable=False)
 
     @declared_attr
-    def gallery(cls):
-        gallery_cls = cls.get_parent_class()
-        gallery_pk = getattr(gallery_cls, gallery_cls.get_pk(), None)
-        return relationship(
-            gallery_cls, foreign_keys=[cls.gallery_id],
-            primaryjoin=lambda: foreign(cls.gallery_id) == remote(gallery_pk),
-            backref='items'
-        )
+    def item_id(cls):
+        item_cls = cls.get_item_class()
+        return Column(Integer,
+                      ForeignKey(item_cls.get_col_pk(),
+                                 ondelete='CASCADE'),
+                      nullable=False)
